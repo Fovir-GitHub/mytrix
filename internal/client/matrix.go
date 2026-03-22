ea03f972bc60cfbe14a6f5bb87cf85e28d2480f6
Fovir <fovir@disroot.org>
2026-03-23 02:09:50 +0800

    feat(client): add `MatrixClient`

---
+++ internal/client/matrix.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Fovir-GitHub/mytrix/internal/crypto"
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/crypto/cryptohelper"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/id"
+)
+
+type MatrixClient struct {
+	c *mautrix.Client
+}
+
+func New(c *mautrix.Client) *MatrixClient {
+	return &MatrixClient{c: c}
+}
+
+func (m *MatrixClient) SendTextMessage(ctx context.Context, roomID id.RoomID, text string) error {
+	content := event.MessageEventContent{
+		MsgType: event.MsgText,
+		Body:    text,
+	}
+
+	_, err := m.c.SendMessageEvent(ctx, roomID, event.EventMessage, content)
+	return err
+}
+
+func (m *MatrixClient) Sync() error {
+	return m.c.Sync()
+}
+
+func (m *MatrixClient) VerifyWithRecoveryKey() error {
+	ch, ok := m.c.Crypto.(*cryptohelper.CryptoHelper)
+	if !ok {
+		return fmt.Errorf("crypto helper type mismatch")
+	}
+
+	return crypto.VerifyWithRecoveryKey(ch.Machine())
+}
+
+func (m *MatrixClient) UserID() id.UserID {
+	return m.c.UserID
+}
---
+++ internal/service/message.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Fovir-GitHub/mytrix/internal/client"
+	"maunium.net/go/mautrix/event"
+)
+
+type MessageService struct {
+	client *client.MatrixClient
+}
+
+func NewMessageService(c *client.MatrixClient) *MessageService {
+	return &MessageService{client: c}
+}
+
+func (s *MessageService) HandleMessage(ctx context.Context, evt *event.Event) {
+	content := evt.Content.AsMessage()
+	if content.MsgType != event.MsgText || evt.Sender == s.client.UserID() {
+		return
+	}
+
+	if content.Body == "!ping" {
+		_ = s.client.SendTextMessage(ctx, evt.RoomID, "pong")
+		return
+	}
+
+	_ = s.client.SendTextMessage(ctx, evt.RoomID, "Feedback")
+}
