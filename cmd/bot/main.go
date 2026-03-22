7b8eaee94d5b0d26e71bf7803139c333eb19cfd0
Fovir <fovir@disroot.org>
2026-03-23 02:23:47 +0800

    feat: add the main process

---
+++ cmd/bot/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/Fovir-GitHub/mytrix/internal/bot"
+	"github.com/Fovir-GitHub/mytrix/internal/config"
+	"github.com/Fovir-GitHub/mytrix/internal/logger"
+	_ "maunium.net/go/mautrix/crypto/goolm"
+)
+
+func main() {
+	err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.Init()
+	slog.Info("main start")
+
+	b, err := bot.New()
+	if err != nil {
+		slog.Error(
+			"failed to create bot",
+			"err", err,
+		)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	if err = b.Start(ctx); err != nil {
+		slog.Error(
+			"fail to run bot",
+			"err", err,
+		)
+		os.Exit(1)
+	}
+}
