f8bd2e6fcdb8bf886224648a278745a02b737f5e
Fovir <fovir@disroot.org>
2026-03-27 14:52:56 +0800

    feat(handler): add websocket handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/Fovir-GitHub/mytrix/internal/model"
 	"github.com/Fovir-GitHub/mytrix/internal/service"
 	"maunium.net/go/mautrix/event"
 )
@@ -10,14 +12,18 @@ import (
 type Handler struct {
 	service  *service.Service
 	commands map[string]func(context.Context, *event.Event) error
+	events   map[string]func(context.Context, *model.WsEvent) error
 }
 
 func NewHandler(s *service.Service) *Handler {
+	slog.Debug("create handler")
 	h := &Handler{
 		service:  s,
 		commands: make(map[string]func(context.Context, *event.Event) error),
+		events:   make(map[string]func(context.Context, *model.WsEvent) error),
 	}
-	h.registerCommands()
 
+	h.registerCommands()
+	h.registerWSHandler()
 	return h
 }
---
+++ internal/handler/ws.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/Fovir-GitHub/mytrix/internal/model"
+)
+
+func (h *Handler) registerWSHandler() {
+	h.events[model.SourceGotify] = h.handleGotify
+	slog.Info("websocket handler registered")
+}
+
+func (h *Handler) HandleWSEvent(ctx context.Context, event *model.WsEvent) error {
+	for name, handler := range h.events {
+		if event.Source == name {
+			slog.Debug(
+				"handle event",
+				"name", name,
+			)
+			return handler(ctx, event)
+		}
+	}
+	return nil
+}
