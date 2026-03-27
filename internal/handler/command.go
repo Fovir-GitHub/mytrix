666fe14f3d5d2cb6e7fa7cec89c59da1ea97ec75
Fovir <fovir@disroot.org>
2026-03-27 13:46:50 +0800

    refactor(handler): move `registerCommands()` and `HandleCommand()` functions to a single file

---
+++ internal/handler/command.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"context"
+	"log/slog"
+	"strings"
+
+	"maunium.net/go/mautrix/event"
+)
+
+func (h *Handler) registerCommands() {
+	h.commands["!ping"] = h.handlePing
+}
+
+func (h *Handler) HandleCommand(ctx context.Context, evt *event.Event) {
+	content := evt.Content.AsMessage()
+	if content.MsgType != event.MsgText {
+		return
+	}
+
+	if evt.Sender == h.service.Message.UserID() {
+		slog.Debug("receive own message, skipped")
+		return
+	}
+
+	body := strings.TrimSpace(content.Body)
+	slog.Debug(
+		"received text message",
+		"room", evt.RoomID,
+		"sender", evt.Sender,
+		"body", body,
+	)
+
+	for prefix, handler := range h.commands {
+		if strings.HasPrefix(body, prefix) {
+			if err := handler(ctx, evt); err != nil {
+				slog.Error(
+					"handle error",
+					"err", err,
+				)
+			}
+			return
+		}
+	}
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"context"
-	"log/slog"
-	"strings"
 
 	"github.com/Fovir-GitHub/mytrix/internal/service"
 	"maunium.net/go/mautrix/event"
@@ -14,11 +12,6 @@ type Handler struct {
 	commands map[string]func(context.Context, *event.Event) error
 }
 
-func (h *Handler) registerCommands() {
-	h.commands["!ping"] = h.handlePing
-	h.commands["!gotify"] = h.handleGotify
-}
-
 func NewHandler(s *service.Service) *Handler {
 	h := &Handler{
 		service:  s,
@@ -27,36 +20,4 @@ func NewHandler(s *service.Service) *Handler {
 	h.registerCommands()
 
 	return h
-}
-
-func (h *Handler) Handle(ctx context.Context, evt *event.Event) {
-	content := evt.Content.AsMessage()
-	if content.MsgType != event.MsgText {
-		return
-	}
-
-	if evt.Sender == h.service.Message.UserID() {
-		slog.Debug("receive own message, skipped")
-		return
-	}
-
-	body := strings.TrimSpace(content.Body)
-	slog.Debug(
-		"received text message",
-		"room", evt.RoomID,
-		"sender", evt.Sender,
-		"body", body,
-	)
-
-	for prefix, handler := range h.commands {
-		if strings.HasPrefix(body, prefix) {
-			if err := handler(ctx, evt); err != nil {
-				slog.Error(
-					"handle error",
-					"err", err,
-				)
-			}
-			return
-		}
-	}
 }
