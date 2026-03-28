014ccfe2bec67df88f6de0d65ad047c96a490f1e
Fovir <fovir@disroot.org>
2026-03-28 17:31:34 +0800

    refactor(client): refctor the websocket client

--- internal/ws/client.go
+++ internal/ws/client.go
@@ -2,6 +2,7 @@ package ws
 
 import (
 	"log/slog"
+	"sync"
 	"time"
 
 	"github.com/Fovir-GitHub/mytrix/internal/config"
@@ -12,12 +13,15 @@ type Client struct {
 	url  string
 	conn *websocket.Conn
 	recv chan []byte
+	done chan struct{}
+	mu   sync.Mutex
 }
 
 func NewClient(url string) *Client {
 	return &Client{
 		url:  url,
 		recv: make(chan []byte, config.Config.WS.RecvBufferSize),
+		done: make(chan struct{}),
 	}
 }
 
@@ -25,9 +29,26 @@ func (c *Client) Start() {
 	go c.connectLoop()
 }
 
+func (c *Client) Stop() {
+	close(c.done)
+	c.mu.Lock()
+	if c.conn != nil {
+		if err := c.conn.Close(); err != nil {
+			slog.Error("websocket stop close failed", "err", err)
+		}
+	}
+	c.mu.Unlock()
+}
+
 func (c *Client) connectLoop() {
 	retryInterval := time.Duration(config.Config.WS.RetryInterval)
 	for {
+		select {
+		case <-c.done:
+			return
+		default:
+		}
+
 		conn, _, err := websocket.DefaultDialer.Dial(c.url, nil)
 		if err != nil {
 			slog.Error(
@@ -42,23 +63,44 @@ func (c *Client) connectLoop() {
 			"url", c.url,
 		)
 
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				slog.Error(
-					"websocket read error",
-					"err", err,
-				)
-				conn.Close()
-				break
-			}
-			slog.Debug(
-				"websocket message received",
-				"msg", string(msg),
-			)
-			c.recv <- msg
+		c.mu.Lock()
+		c.conn = conn
+		c.mu.Unlock()
+
+		if !c.readLoop(conn) {
+			return
 		}
 		time.Sleep(retryInterval * time.Second)
+	}
+}
+
+func (c *Client) readLoop(conn *websocket.Conn) bool {
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			slog.Error(
+				"websocket read error",
+				"err", err,
+			)
+			if err := conn.Close(); err != nil {
+				slog.Error("websocket connection close failed", "err", err)
+			}
+			return true
+		}
+		slog.Debug(
+			"websocket message received",
+			"msg", string(msg),
+		)
+		select {
+		case c.recv <- msg:
+		case <-c.done:
+			if err := conn.Close(); err != nil {
+				slog.Error("websocket connection close failed", "err", err)
+			}
+			return false
+		default:
+			slog.Warn("recv channel full, dropping message")
+		}
 	}
 }
 
