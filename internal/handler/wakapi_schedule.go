358670ce00bea63a6458f7d1d8fb556ea7d0abe4
Fovir <fovir@disroot.org>
2026-04-05 05:13:53 +0800

    feat(handler): add wakapi schedule handler

---
+++ internal/handler/wakapi_schedule.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/Fovir-GitHub/mytrix/internal/config"
+	"github.com/Fovir-GitHub/mytrix/internal/model"
+	"github.com/Fovir-GitHub/mytrix/internal/scheduler"
+	"maunium.net/go/mautrix/id"
+)
+
+func (h *Handler) handleWakapiSchedule(ctx context.Context, interval model.WakapiInterval, text string) {
+	roomID := config.Config.RoomID
+	report, err := h.fetchWakapiReport(interval)
+	if err != nil {
+		slog.Error("fetch wakapi report failed", "err", err)
+		return
+	}
+	msg := text + "\n" + report
+	if err := h.service.Message.Reply(ctx, id.RoomID(roomID), msg); err != nil {
+		slog.Error(
+			"send message failed",
+			"report", report,
+			"roomID", roomID,
+			"err", err,
+		)
+	}
+}
+
+func (h *Handler) WakapiScheduleList() []scheduler.ScheduledJob {
+	return []scheduler.ScheduledJob{
+		{
+			Cron: "0 9 * * *",
+			Job:  func() { h.handleWakapiSchedule(context.Background(), model.WakapiIntervalYesterday, "Daily Report") },
+		},
+		{
+			Cron: "0 9 1 * *",
+			Job:  func() { h.handleWakapiSchedule(context.Background(), model.WakapiIntervalLast30Days, "Monthly Report") },
+		},
+		{
+			Cron: "0 9 1 1 *",
+			Job: func() {
+				h.handleWakapiSchedule(context.Background(), model.WakapiIntervalLast12Months, "Yearly Report")
+			},
+		},
+	}
+}
