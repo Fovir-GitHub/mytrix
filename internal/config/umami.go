2f855fe85ea2ad317939afcb2d10c3ad6cfbfe42
Fovir <fovir@disroot.org>
2026-04-08 12:33:34 +0800

    docs(config): add description of Umami configuration

--- internal/config/umami.go
+++ internal/config/umami.go
@@ -7,16 +7,26 @@ import (
 )
 
 type UmamiConfig struct {
-	Enabled           bool   `env:"UMAMI_ENABLED" envDefault:"false"`
-	Server            string `env:"UMAMI_SERVER"`
-	Username          string `env:"UMAMI_USERNAME"`
-	Password          string `env:"UMAMI_PASSWORD"`
-	DefaultInterval   string `env:"UMAMI_DEFAULT_INTERVAL" envDefault:"daily"`
-	Format            string `env:"UMAMI_FORMAT" envDefault:"- {{.Name}} - {{.Domain}}\n\t- Visitors: {{.Visitors}}\n\t- Visits: {{.Visits}}\n\tBounces Rate: {{.BouncesRate}}"`
-	DailyReportCron   string `env:"UMAMI_DAILY_REPORT_CRON" envDefault:"0 9 * * *"`
-	WeeklyReportCron  string `env:"UMAMI_WEEKLY_REPORT_CRON" envDefault:"0 9 * * 1"`
+	// Enabled determines whether to enable Umami integration.
+	Enabled bool `env:"UMAMI_ENABLED" envDefault:"false"`
+	// Server sets the server of Umami.
+	Server string `env:"UMAMI_SERVER"`
+	// Username sets the user to fetch data from.
+	Username string `env:"UMAMI_USERNAME"`
+	// Password stores the user's password for auth.
+	Password string `env:"UMAMI_PASSWORD"`
+	// DefaultInterval sets the default query interval used in commands.
+	DefaultInterval string `env:"UMAMI_DEFAULT_INTERVAL" envDefault:"daily"`
+	// Format sets the format of Umami reports.
+	Format string `env:"UMAMI_FORMAT" envDefault:"- {{.Name}} - {{.Domain}}\n\t- Visitors: {{.Visitors}}\n\t- Visits: {{.Visits}}\n\tBounces Rate: {{.BouncesRate}}"`
+	// DefaultInterval sets the time to send Umami daily report.
+	DailyReportCron string `env:"UMAMI_DAILY_REPORT_CRON" envDefault:"0 9 * * *"`
+	// WeeklyReportCron sets the time to send Umami weekly report.
+	WeeklyReportCron string `env:"UMAMI_WEEKLY_REPORT_CRON" envDefault:"0 9 * * 1"`
+	// MonthlyReportCron sets the time to send Umami monthly report.
 	MonthlyReportCron string `env:"UMAMI_MONTHLY_REPORT_CRON" envDefault:"0 9 1 * *"`
-	YearlyReportCron  string `env:"UMAMI_YEARLY_REPORT_CRON" envDefault:"0 9 1 1 *"`
+	// YearlyReportCron sets the time to send Umami yearly report.
+	YearlyReportCron string `env:"UMAMI_YEARLY_REPORT_CRON" envDefault:"0 9 1 1 *"`
 }
 
 func (mc *MytrixConfig) validateUmami() error {
