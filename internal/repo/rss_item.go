96f79458e29de2cdb59ce9fe3978b6066ffda6b9
Fovir <fovir@disroot.org>
2026-04-16 23:11:01 +0800

    fix(repo): fix the soft-delete problem

--- internal/repo/rss_item.go
+++ internal/repo/rss_item.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	"errors"
 	"fmt"
 
 	"codeberg.org/Fovir/mytrix/internal/model"
@@ -21,9 +22,33 @@ func NewRSSItemRepo(db *gorm.DB) *RSSItemRepo {
 
 // Create persists the given RSSItem to the database.
 // It uses GORM's Create method and returns any error encountered.
+// It also checks items soft-deleted before.
+// If the item to be added exists but soft-deleted, it will restore it.
 func (r *RSSItemRepo) Create(item *model.RSSItem) error {
-	if err := r.db.Create(item).Error; err != nil {
-		return fmt.Errorf("create feed item failed (id=%d): %w", item.ID, err)
+	var existing model.RSSItem
+	err := r.db.Unscoped().Where("feed_id = ? AND guid = ?", item.FeedID, item.GUID).First(&existing).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		if err := r.db.Create(item).Error; err != nil {
+			return fmt.Errorf("create feed item failed (guid=%s): %w", item.GUID, err)
+		}
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("query existing rss item failed (guid=%s): %w", item.GUID, err)
+	}
+
+	if existing.DeletedAt.Valid {
+		existing.DeletedAt = gorm.DeletedAt{}
+		if err := r.db.Unscoped().Save(&existing).Error; err != nil {
+			return fmt.Errorf("save rss item failed (guid=%s): %w", item.GUID, err)
+		}
+	}
+	return nil
+}
+
+func (r *RSSItemRepo) DeleteByFeedId(feedId int) error {
+	if err := r.db.Where("feed_id = ?", feedId).Delete(&model.RSSItem{}).Error; err != nil {
+		return fmt.Errorf("delete rss item failed (feed_id=%d), %w", feedId, err)
 	}
 	return nil
 }
