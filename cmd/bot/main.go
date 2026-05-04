5b289694cebc1e9fb5b846ca30ad2aa477208aac
Fovir <fovir@disroot.org>
2026-05-04 13:06:47 +0800

    chore: bump to v0.2.6

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -19,7 +19,7 @@ func main() {
 		log.Fatal(err)
 	}
 	logger.Init()
-	slog.Info("mytrix started", "version", "v0.2.5")
+	slog.Info("mytrix started", "version", "v0.2.6")
 	config.SetTimeZone()
 	model.InitTemplates()
 
