759f8a371099ff22c674e757e786599658277178
Fovir <fovir@disroot.org>
2026-05-31 01:29:10 +0800

    feat(config): add max length of a message option
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/config/msg.go
+++ internal/config/msg.go
@@ -5,4 +5,6 @@ type MsgConfig struct {
 	AllowMarkdown bool `env:"MSG_ALLOW_MARKDOWN" envDefault:"true"`
 	// AllowHTML determines whether to allow HTML content in messages.
 	AllowHTML bool `env:"MSG_ALLOW_HTML" envDefault:"false"`
+	// DefaultMaxLength sets the default or fallback max length of a message.
+	DefaultMaxLength int `env:"MSG_DEFAULT_MAX_LENGTH" envDefault:"4096"`
 }
