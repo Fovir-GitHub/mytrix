698786992a6f93724e9d86eaf6fb9444c4eb2c28
Fovir <fovir@disroot.org>
2026-07-25 20:13:41 +0800

    feat: add `event` schema
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/assets/sql/schema/room.sql
+++ internal/assets/sql/schema/room.sql
@@ -2,3 +2,10 @@ CREATE TABLE IF NOT EXISTS room (
     id TEXT PRIMARY KEY,
     state TEXT NOT NULL CHECK (state IN ('left', 'joined'))
 );
+
+CREATE TABLE IF NOT EXISTS event (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id TEXT NOT NULL,
+    room_id TEXT NOT NULL REFERENCES room (id),
+    UNIQUE (event_id, room_id)
+);
